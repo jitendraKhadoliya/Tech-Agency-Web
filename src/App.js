9422cbd9cc31bdb686e5f3b5d4612bfd7f331c7a
jitendraKhadoliya <jitendrasaini352jjnu@gmail.com>
2022-12-18 01:57:09 +0530

    facing error in Header file and dont know what it is

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import {BrowserRouter as Router , Routes , Route} from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+import './styles/app.scss';
 
 function App() {
   return (
     <Router>
 
+        <Header />
+
       <Routes>
 
         <Route />
-        
+
       </Routes>
+      <Footer />
     </Router>
   );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+const Header = () => {
+
+  return (
+    <>
+{/* 
+        <h1>fsdkj</h1>  
+  
+  
+       <nav>
+        <span>Jinix Services</span>
+
+        <span>
+          <link>ladslk</link>
+        </span>
+         
+          <li>
+          <link> Home</link>
+          
+        </li>
+    
+        
+        
+       </nav> */}
+    </>
+  
+  
+  )
+}
+
+export default Header;
