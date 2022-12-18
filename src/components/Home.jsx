4a9e8e01f63e69a9eed5e2a696e7d5db0d3bb356
jitendraKhadoliya <jitendrasaini352jjnu@gmail.com>
2022-12-19 00:59:12 +0530

    on home page working

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,29 @@
 import React from "react";
+import vg from '../assets/2.webp';
 
 const Home = ()=>{
-    return <div className="home">
+    return <>
+    <div className="home">
             <main>
                 <h2>Jinix Servies</h2>
                 <p>solution for all your Web services</p>
             </main>
+    </div>
+
+    <div className="home2">
+        <img src= {vg} alt="GraphicImage" />
+        <div>
+
+            <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corporis est obcaecati culpa expedita fuga blanditiis voluptatum nostrum ea tempore rerum eum quia quasi, eveniet facilis fugiat! Libero error sint reiciendis.
+                
+            </p>
+
         </div>
+    </div>
+
+
+    </> 
 }
 
 export default Home;
