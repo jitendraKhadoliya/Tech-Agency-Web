6d25444f11a953f90c094f2e754396fd1061050e
jitendra <jitendrasaini352jjnu@gmail.com>
2023-04-10 23:09:19 +0530

    updated web app theme

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,26 +1,23 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
-import {HashLink} from 'react-router-hash-link'
+import React from "react";
+import { Link } from "react-router-dom";
+import { HashLink } from "react-router-hash-link";
 
 const Header = () => {
-
   return (
     <nav>
       <h1>Jinix Services</h1>
 
-        <ul>
-          <li>
-            <HashLink to={"/#home"}>Home</HashLink>
-            <Link to={"/contact"}>Contact</Link>
-            <HashLink to = {"/#about"}>About</HashLink>
-            <HashLink to={"/#brands"}>Brands</HashLink>
-            <Link to={"/services"}>Services</Link>
-          </li>
-        </ul>
-
+      <ul>
+        <li>
+          <HashLink to={"/#home"}>Home</HashLink>
+          <Link to={"/contact"}>Contact</Link>
+          <HashLink to={"/#about"}>About</HashLink>
+          <HashLink to={"/#brands"}>Brands</HashLink>
+          <Link to={"/services"}>Services</Link>
+        </li>
+      </ul>
     </nav>
-
-  )
-}
+  );
+};
 
 export default Header;
